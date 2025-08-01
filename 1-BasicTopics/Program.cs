1d1e1eaa027f08b6bd6c72523962b8965eb21e41
zharifzubaidi <zharif96@gmail.com>
2025-08-01 16:28:58 +0800

    add: topic 1 ongoing

---
+++ 1-BasicTopics/Program.cs
@@ -0,0 +1,48 @@
+﻿// Basic topics in C#
+
+string? userInput = ""; // declare the string to expect a null value
+Console.Write("Please select your topic: ");
+userInput = Console.ReadLine();
+
+if (userInput != null)
+{
+    if (userInput == "1")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tTopic 1:");
+        Console.WriteLine("*****************************");
+
+        // Prints out a message to the console
+        Console.WriteLine("Hello world");
+
+        // Takes user input and load it as a string
+        string? userInput2 = Console.ReadLine();
+        Console.WriteLine($"You entered: {userInput2}");
+
+
+    }
+    else if (userInput == "2")
+    {
+        // Convert from if else to switch
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tTopic 2:");
+        Console.WriteLine("*****************************");
+
+    }
+    else if (userInput == "3")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tTopic 3:");
+        Console.WriteLine("*****************************");
+
+    }
+
+    Console.WriteLine("Thank you! Please press enter to exit.");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Please try again");
+}
