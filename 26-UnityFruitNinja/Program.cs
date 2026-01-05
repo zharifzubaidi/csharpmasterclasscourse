c81f932072b2d2047d22104ec8660c3ffcf4ff37
zharifzubaidi <zharif96@gmail.com>
2026-01-05 20:17:10 +0800

    add: new topics and start chapter 10

---
+++ 26-UnityFruitNinja/Program.cs
@@ -0,0 +1,141 @@
+﻿
+namespace UnityFruitNinjaExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? userInput = "";
+            Console.Write("Please select your example: ");
+            userInput = Console.ReadLine();
+
+            if (userInput != null)
+            {
+                if (userInput == "1")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+
+                }
+                else if (userInput == "2")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "3")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "4")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "5")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "6")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "7")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "8")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "9")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "10")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "11")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "12")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "13")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "14")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "15")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else if (userInput == "16")
+                {
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine($"\tExample {userInput}:");
+                    Console.WriteLine("*****************************");
+
+                }
+                else
+                {
+
+                }
+
+                Console.WriteLine("Thank you! Please press enter to exit.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Please try again");
+            }
+        }
+    }
+}
